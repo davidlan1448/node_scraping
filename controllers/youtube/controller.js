21fec20a150bd2c0015098a0b20c84cb80d6cf90
David Eduardo Ramirez Soto <david.ramirez1448@gmail.com>
2020-01-19 08:46:49 -0400

    agregada la ruta para obtener video de youtube con sus comentarios

--- controllers/youtube/controller.js
+++ controllers/youtube/controller.js
@@ -1,5 +1,17 @@
+var rp = require('request-promise');
+const cherrio = require('cheerio');
 const { requestYoutube } = require('../../util/requestScrappig');
 const Response = require('../../util/Response');
+var scraper = require("youtube-comment-scraper");
+
+const config = function (url) {
+    return {
+        uri: url,
+        transform: function (body) {
+            return cherrio.load(body);
+        }
+    }
+}
 
 /**
  * @description obtiene el scrappig de youtube
@@ -18,4 +30,33 @@ const getScrappig = async (req, res) => {
     }
 }
 
-module.exports = { getScrappig };
+/**
+ * @description get info video 
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getInfoVideo = async (req, res) => {
+    try {
+        const { url } = req.body;
+
+        if (!url) {
+            Response.error(res, 400, 1, null, 'the url does not exist');
+            return;
+        }
+
+        const $ = await rp(config(url));
+
+        const data = await scraper.comments(url);
+        
+        Response.success(res, {
+            title: $('title').text(),
+            description: $('meta[name=description]').attr('content'),
+            ...data
+        }, 'video', 'youtube');
+    } catch (err) {
+        console.error(err);
+        Response.error(res, 500);
+    }
+}
+
+module.exports = { getScrappig, getInfoVideo };
