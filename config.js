ca0f8d9798e880bf20f9a5bc520a31b83abc37b8
David Eduardo Ramirez Soto <david.ramirez1448@gmail.com>
2020-02-02 23:11:41 -0400

    arreglos en el controller de facebook

---
+++ config.js
@@ -0,0 +1,7 @@
+
+const config = {
+    facebookEmail: process.env.FACEBOOK_EMAIL,
+    facebookPassword: process.env.FACEBOOK_PASSWORD
+}
+
+module.exports = config;
--- controllers/facebook/controller.js
+++ controllers/facebook/controller.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 var rp = require('request-promise');
 const cherrio = require('cheerio');
 const Response = require('../../util/Response');
-const { configUrl } = require('../../util/configUrl');
+const { facebookEmail, facebookPassword } = require('../../config');
 
 /**
  * @description obtiene el scrappig de youtube
@@ -12,17 +12,64 @@ const { configUrl } = require('../../util/configUrl');
 const getScrappig = async (req, res) => {
     try {
         const { url } = req.body;
-        const $ = await rp(configUrl('https://store.steampowered.com/app/1160750/Grim_Clicker/'));
-        
-        console.log($('.game_background_glow').html())
+        const browser = await puppeteer.launch({ headless: false,devtools: true, timeout: 0 });
+        const page = await browser.newPage();
+        await page.goto(url,
+            { waitUntil: 'networkidle2', timeout: 100000 }
+        );
 
-        Response.success(res, {
-            description: $('.game_description_snippet').text(),
-            developers: $('.developers_list').text()
+        await page.evaluate(() => {
+            document.querySelector('#expanding_cta_close_button').click();
         });
+
+        await page.evaluate(() => {
+            document.getElementsByClassName('_1whp _4vn2')[3].children[0].click();
+        });
+
+        await page.waitForSelector('._7a94._7a95', { timeout: 80000, visible: true });
+
+        for (let index = 0; index < 10; index++) {
+            await page.evaluate(() => {
+                document.querySelector('._4ssp').click();
+            });
+        }
+
+        await page.waitFor(3000);
+
+
+        const data = await page.evaluate(() => {
+            const selectElement = (element) => document.querySelector(element);
+            const selectElements = (element) => document.querySelectorAll(element);
+            const commentElements = Array.from(selectElement('ul._7a9a').children);
+            
+            const comments = commentElements.map(element => {
+                const content = element.getElementsByClassName('clearfix')[0];
+                return {
+                    image: element.getElementsByClassName('_ohe lfloat')[0].getElementsByTagName('img')[0].src,
+                    name: element.getElementsByClassName('_72vr')[0].children[0].innerText,
+                    comment: element.getElementsByClassName('_72vr')[0].children[1].innerText/* ,
+                    imageComment: element.getElementsByClassName('_42ef')[0].getElementsByClassName('_2txe')[0].children[0].getAttribute('href') || null */
+                }
+            });
+            
+            return {
+                text: selectElement('._5pbx.userContent._3576').innerText.trim(),
+                image: selectElement('._s0._4ooo._6y97._6_ut._5xib._5sq7._44ma._rw.img').src,
+                likes: selectElement('._3dlh._3dli').innerText,
+                user: {
+                    avatar: selectElement('._s0._4ooo._6y97._6_ut._5xib._5sq7._44ma._rw.img').getAttribute('src'),
+                    username: selectElement('.fwb.fcg').innerText
+                },
+                comments
+            }
+        });
+
+        Response.success(res, data, 'comments', 'facebook');
+        await browser.close();
     } catch (error) {
         console.error(error);
         Response.error(res, 500);
+        await browser.close();
     }
 }
 
